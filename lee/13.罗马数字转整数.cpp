c1d053506c3da9b8f7d0aed98829a935b620e8cc
wangjize <y98d225@163.com>
2024-03-05 23:37:36 +0800

    #13 Roman numerals to integer-2

--- lee/13.罗马数字转整数.cpp
+++ lee/13.罗马数字转整数.cpp
@@ -12,6 +12,29 @@ using namespace std;
 class Solution {
 public:
     int romanToInt(string s) {
+        map<char,int> mp;
+        mp.insert(make_pair<char,int>('I',1));
+        mp.insert(make_pair<char,int>('V',5));
+        mp.insert(make_pair<char,int>('X',10));
+        mp.insert(make_pair<char,int>('L',50));
+        mp.insert(make_pair<char,int>('C',100));
+        mp.insert(make_pair<char,int>('D',500));
+        mp.insert(make_pair<char,int>('M',1000));
+
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s[i] >= s[i+1]) //这一步有问题。明天改
+            {
+                sum += mp.at(s[i]);
+            }
+            else
+            {
+                sum -+ mp.at(s[i]);
+            }
+        }
+
+        return sum;   
     }
 };
 // @lc code=end
