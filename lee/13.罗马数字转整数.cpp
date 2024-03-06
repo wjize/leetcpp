b3d6c4560001a05162463684011d0b8423ea5598
wangjize <y98d225@163.com>
2024-03-06 12:17:02 +0800

    #13 Roman numerals to integer-3

--- lee/13.罗马数字转整数.cpp
+++ lee/13.罗马数字转整数.cpp
@@ -5,36 +5,29 @@
  */
 #include <iostream>
 #include <string>
-#include <map>
+#include <unordered_map>
 using namespace std;
 
 // @lc code=start
 class Solution {
 public:
     int romanToInt(string s) {
-        map<char,int> mp;
-        mp.insert(make_pair<char,int>('I',1));
-        mp.insert(make_pair<char,int>('V',5));
-        mp.insert(make_pair<char,int>('X',10));
-        mp.insert(make_pair<char,int>('L',50));
-        mp.insert(make_pair<char,int>('C',100));
-        mp.insert(make_pair<char,int>('D',500));
-        mp.insert(make_pair<char,int>('M',1000));
-
+        /*
+            map有序，底层红黑树
+            unordered_map无序，底层哈希表
+        */
+        //unordered_map<char,int> mp = {{'M', 1000}, {'D', 500}, {'C', 100}, {'L', 50}, {'X', 10}, {'V', 5}, {'I', 1}};
+        unordered_map<char,int> mp = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
         int sum = 0;
-        for (int i = 0; i < s.length(); i++)
+        for (int i = 0; i < s.size(); i++)
         {
-            if (s[i] >= s[i+1]) //这一步有问题。明天改
-            {
-                sum += mp.at(s[i]);
-            }
+            if (mp[s[i]] >= mp[s[i+1]]) 
+                sum += mp[s[i]];
             else
-            {
-                sum -+ mp.at(s[i]);
-            }
+                sum -= mp[s[i]];
         }
-
-        return sum;   
+        //MCMXCIV
+        return sum; 
     }
 };
 // @lc code=end
