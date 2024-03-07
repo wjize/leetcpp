1ba3b7b59e617c729cad39bba84c79be3d327064
wangjize <y98d225@163.com>
2024-03-07 17:57:43 +0800

    new test

--- test/main.cpp
+++ test/main.cpp
@@ -4,39 +4,27 @@
 #include <map>
 using namespace std;
 
-void test(vector<string>& strs){
-        string s = "";
-        if (strs.empty()) {
-            std::cout << s << std::endl;
+void test(vector<int>& nums){
+        //nums.erase(unique(nums.begin(),nums.end()),nums.end());
+        vector<int>::iterator ii = unique(nums.begin(),nums.end());
+        std::cout << "ii: " << *ii << std::endl;
+        for (vector<int>::iterator it = nums.begin(); it != nums.end(); ++it)
+        {
+            std::cout << "size: " << nums.size() << std::endl;
+            std::cout << "it: " << *it << std::endl;
         }
-        // for (vector<string>::iterator it = strs.begin(); it != strs.end(); ++it)
-        // {
-        //     /* code */
-        //     int i = 0;
-        //     if ((it[i] == it[i+1]) && (it[i] == it[i+2]))
-        //     {
-        //         s += it[i];
-        //     }
-        //     i++;
-        // }
-        int i = 0;
-        for (vector<string>::iterator it = strs.begin(); it != strs.end(); ++it) {
-            for (vector<string>::iterator it2 = it + 1; it2 != strs.end(); ++it2) {
-                if (i >= it->size() || i >= it2->size() || (*it)[i] != (*it2)[i]) {
-                    std::cout << s << std::endl;
-                }
-            }
-            s += (*it)[i];
-            ++i;
-        }
-        std::cout << s << std::endl;
+        
+        //std::cout << s << std::endl;
 }
 
 int main() {
-    vector<string> strs;
-    strs.push_back("flower");
-    strs.push_back("flow");
-    strs.push_back("flight");
+    vector<int> strs;
+    strs.push_back(1);
+    strs.push_back(1);
+    strs.push_back(2);
+    strs.push_back(2);
+    strs.push_back(2);
+    strs.push_back(3);
     // = ["flower","flow","flight"];
     test(strs);
     return 0;
