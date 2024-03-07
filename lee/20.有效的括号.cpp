6b5b942f4f869982d6470872718adfc1e25eb286
wangjize <y98d225@163.com>
2024-03-07 14:42:37 +0800

    #20 valid parentheses-2

--- lee/20.有效的括号.cpp
+++ lee/20.有效的括号.cpp
@@ -16,19 +16,26 @@ public:
         if (s.size() % 2 != 0)
             return false;
         vector<char> left = {'(','[','{'};
+        //vector<char> right = {')',']','}'};
         vector<string> str = {"()","[]","{}"};
         stack<char> mystack;
 
         for (int i = 0; i < s.size(); i++)
         {
+            /*
+            if (left[s[i]])
+                mystack.push(s[i]);
+            */
             if (find(left.begin(),left.end(),s[i]) != left.end())
                 mystack.push(s[i]);
             else
             {
                 if (mystack.empty())
                     return false;
+                //string mach_str = to_string(mystack.top() + s[i]);
                 string mach_str = string(1,mystack.top()) + s[i];
                 mystack.pop();
+                //vector<string>::iterator it = std::find(str.begin(), str.end(), mach_str);
                 if (find(str.begin(), str.end(), mach_str) != str.end())
                     continue;
                 else
